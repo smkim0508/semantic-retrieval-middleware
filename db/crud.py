7b082611df60d8883dacaf461d57df9fc2fb441a
Sungmin Kim <sk3378@princeton.edu>
2026-02-26 11:42:44 -0500

    update: simple vector db ORM initialized w/ declarative base

--- db/crud.py
+++ db/crud.py
@@ -1,1 +1,4 @@
 # CRUD operations on the vector db
+from db.model import VectorDB
+
+# TODO: implement simple cruds
--- db/model.py
+++ db/model.py
@@ -1,1 +1,21 @@
 # baseline ORM model for the vector db table using SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Text, DateTime, Index, Column, Integer
+from pgvector.sqlalchemy import Vector
+from datetime import datetime
+from typing import Optional
+
+# A dedicated Base for all models that map to tables in the main database.
+class MainDB_Base(DeclarativeBase):
+    pass
+
+class VectorDB(MainDB_Base):
+    """
+    Simple vector db table, used to experiment with semantic retrieval
+    """
+    __tablename__ = "vector_db"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    vector = Column(Vector(1536), nullable=False)
+    text = Column(Text, nullable=False)
+
+    # TODO: define index if needed based on testing
