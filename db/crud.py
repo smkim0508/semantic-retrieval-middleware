e923c110c6a51bc98c248e39515d49edc2374d63
Sungmin Kim <sk3378@princeton.edu>
2026-02-26 14:08:50 -0500

    update: basic helper to store vector to db

--- db/crud.py
+++ db/crud.py
@@ -1,4 +1,21 @@
 # CRUD operations on the vector db
 from db.model import VectorDB
 
-# TODO: implement simple cruds
+# using sync session for testing now, async TBD
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from sqlalchemy.engine import Engine
+
+engine = create_engine("postgresql+psycopg2://user:pass@localhost/db")
+
+def store_vector(vector: list[float], text: str, engine: Engine) -> VectorDB:
+    """
+    Takes a text string and its vector embedding and stores it in the vector db
+    """
+    obj = VectorDB(vector=vector, text=text)
+    with sessionmaker(engine)() as session:
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+    return obj
